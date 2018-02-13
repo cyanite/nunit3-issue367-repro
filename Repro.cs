8c689c5fec3b9178fe2a0a8f644e5a4494594b70
Sune Foldager <cyano@me.com>
2018-02-13 15:35:32 +0100

    initial version

---
+++ Repro.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Repro
+{
+  public class TestClass
+  {
+    [Test]
+    public void Test()
+    {
+      var x = 1;
+      Assert.True(2 * x == 2);
+    }
+  }
+}
